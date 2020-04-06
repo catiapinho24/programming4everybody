91affc8dab1bbb494c78e89c3fa0916c316bf519
Gabriele Canepa <git@gabrielecanepa.com>
2020-04-06 17:25:09 +0200

    Update hello.rb

--- test/hello.rb
+++ test/hello.rb
@@ -1,4 +1,4 @@
-# Hello there! I'm your first Ruby file 🎉 Run me from your terminal with `ruby test/terminal.rb`
+# Hello there, I'm your first Ruby file! 🎉 Run me from your terminal with `ruby test/hello.rb`
 
 puts "Welcome to the course!"
 puts "Let's play with some code 😎"
